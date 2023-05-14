ea8fd1ae41612887f736fe05de13f5b72cd8a317
1-vlad-1 <vlad01022002@mail.ru>
2023-05-14 14:33:10 +0300

    4th commit to master

--- code_1.py
+++ code_1.py
@@ -1,6 +1,6 @@
 def capitalize(String):
     return String.title()
-capitalize("shop") # [Shop]
+capitalize("snow") # [Snow] берем из обоих веток
 capitalize("python programming") # [Python Programming]
 capitalize("how are you!") # [How Are You!]
 
