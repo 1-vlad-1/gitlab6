292f4a4121b1591c10869b8b34b266d7b6349dd9
1-vlad-1 <vlad01022002@mail.ru>
2023-05-14 14:26:40 +0300

    first commit to master

---
+++ code_1.py
@@ -0,0 +1,24 @@
+def capitalize(String):
+    return String.title()
+capitalize("shop") # [Shop]
+capitalize("python programming") # [Python Programming]
+capitalize("how are you!") # [How Are You!]
+
+def merge(dic1,dic2):
+    dic3=dic1.copy()
+    dic3.update(dic2)
+    return dic3
+dic1={1:"hello", 2:"world"}
+dic2={3:"Python", 4:"Programming"}
+merge(dic1,dic2) # {1: 'hello', 2: 'world', 3: 'Python', 4: 'Programming'}
+
+import time
+start_time= time.time()
+def fun():
+    a=2
+    b=3
+    c=a+b
+end_time= time.time()
+fun()
+timetaken = end_time - start_time
+print("Your program takes: ", timetaken)
